05336cac5ad97e153d93d3e7a2c1942433910b0c
unknown <michaelxaviercanonizado@gmail.com>
2024-07-21 16:40:54 +0800

    chore: add typography classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
-    <div className=''>
-      <h1 className='text-2xl'>Hello World!</h1>
+    <div className='space-y-8'>
+      <div className='max-w-[500px]'>
+        <h1 className='text-display'>
+          Creator. Storyteller. <span className=''>Webflow.</span>
+        </h1>
+      </div>
+      <div className='max-w-[500px]'>
+        <h2 className='text-heading'>Experience. Experience.</h2>
+      </div>
+      <div className='max-w-[500px]'>
+        <p className='text-body'>
+          Creating a digital experience that bridge aesthetics and functionality
+        </p>
+      </div>
+      <div className='max-w-[500px]'>
+        <p className='text-sub text-muted-foreground'>
+          michaelxaviercanonizado@gmail.com. mikeycanonizado@gmail.com
+        </p>
+      </div>
     </div>
   );
 }
