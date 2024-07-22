46697edbfbe8ebb99be169a88575868e9836770f
unknown <michaelxaviercanonizado@gmail.com>
2024-07-22 18:46:52 +0800

    feat: add <Introduction/> section

--- src/app/_sections/introduction.tsx
+++ src/app/_sections/introduction.tsx
@@ -3,7 +3,24 @@ import { ComponentBaseProps } from '@/types';
 import React from 'react';
 
 const Introduction = ({ className }: ComponentBaseProps) => {
-  return <section className={cn('container', className)}>Introduction</section>;
+  return (
+    <section
+      className={cn('container space-y-md border-b-[1px] p-xl', className)}
+    >
+      <div className='max-w-[700px]'>
+        <h1 className='text-heading'>
+          Looking for a developer who can create a visually appealing and fully
+          functional website?
+        </h1>
+      </div>
+      <div className='max-w-[700px]'>
+        <p className='text-body'>
+          Your search ends here! With my knowledge in web development,
+          you&apos;ll get a website that sets you apart!
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Introduction;
