adcb89a1dfe578d35691a9d3e93744a5011b2efe
unknown <michaelxaviercanonizado@gmail.com>
2024-07-23 00:25:14 +0800

    feat: rework <Hero/>

--- src/app/_sections/hero.tsx
+++ src/app/_sections/hero.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import HeroName from '@/../public/hero-name';
 
 const Hero = () => {
   return (
     <>
-      <section className='hero-height flex flex-col items-center justify-center overflow-hidden text-center'>
-        <div className='mb-lg'>
+      <section className='hero-height flex w-full flex-col justify-end overflow-hidden pb-md text-center'>
+        <div className='flex grow flex-col items-center justify-center space-y-md'>
+          <div className='text-heading space-y-[-8px]'>
+            <div>
+              <p className=''>creator.</p>
+            </div>
+            <div>
+              <p className=''>storyteller.</p>
+            </div>
+            <div>
+              <p className=''>web developer.</p>
+            </div>
+          </div>
+          <div className=''>
+            <p className='text-body'>
+              Creating a digital experience that bridge aesthetics and
+              functionality
+            </p>
+          </div>
+        </div>
+        <div className=''>
+          <HeroName />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
+
+{
+  /* <div className='mb-lg'>
           <div className='size-[72px] rounded-full bg-foreground' />
         </div>
         <div className='mb-[-16px]'>
@@ -20,9 +51,11 @@ const Hero = () => {
             Creating a digital experience that bridge aesthetics and
             functionality
           </p>
-        </div>
-      </section>
-      <div className='text-sub container !m-0 grid h-[64px] grid-cols-3 border-b border-t bg-background !py-0'>
+        </div> */
+}
+
+{
+  /* <div className='text-sub container !m-0 grid h-[64px] grid-cols-3 border-b border-t bg-background !py-0'>
         <div className='flex items-center justify-start'>
           <p className=''>{'// Code & Engage'}</p>
         </div>
@@ -34,9 +67,5 @@ const Hero = () => {
         <div className='flex items-center justify-end'>
           <p className=''>michaelxaviercanonizado@gmail.com</p>
         </div>
-      </div>
-    </>
-  );
-};
-
-export default Hero;
+      </div> */
+}
