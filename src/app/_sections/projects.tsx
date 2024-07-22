85e16888258f58b31e76a7baa5f0e3317dbc6c28
unknown <michaelxaviercanonizado@gmail.com>
2024-07-22 21:42:06 +0800

    feat: add <Projects/> title

--- src/app/_sections/projects.tsx
+++ src/app/_sections/projects.tsx
@@ -3,7 +3,11 @@ import { ComponentBaseProps } from '@/types';
 
 const Projects = ({ className }: ComponentBaseProps) => {
   return (
-    <section className={cn('container border-b', className)}>Projects</section>
+    <section className={cn('container border-b', className)}>
+      <div>
+        <h1 className='text-heading text-center'>Projects</h1>
+      </div>
+    </section>
   );
 };
 
