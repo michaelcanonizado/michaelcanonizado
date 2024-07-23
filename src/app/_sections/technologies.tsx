ba8b18ea48d896d928b4a070e3b05d06006513eb
unknown <michaelxaviercanonizado@gmail.com>
2024-07-23 18:49:11 +0800

    feat: add base <Badge/> styles and compound components

--- src/app/_sections/technologies.tsx
+++ src/app/_sections/technologies.tsx
@@ -1,17 +1,33 @@
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
 import { TextDisplay } from '@/components/ui/text';
+import Badge from '@/components/ui/badge';
+import Image from 'next/image';
 
 const Technologies = ({ className }: ComponentBaseProps) => {
   return (
     <section
       className={cn(
-        'min-h-[40vh] w-full border bg-foreground py-xl',
+        'min-h-[40vh] w-full space-y-lg border bg-foreground px-lg py-xl',
         className
       )}
     >
       <div className='flex flex-row items-center justify-center'>
         <TextDisplay className='text-background'>Technologies</TextDisplay>
+      </div>
+      <div className='flex flex-row gap-md'>
+        <Badge className='text-background'>
+          <Badge.Image>
+            <div className='h-full w-full rounded-full bg-background' />
+          </Badge.Image>
+          <Badge.Title>NextJS</Badge.Title>
+        </Badge>
+        <Badge className='text-background'>
+          <Badge.Image>
+            <div className='h-full w-full rounded-full bg-background' />
+          </Badge.Image>
+          <Badge.Title>Typescript</Badge.Title>
+        </Badge>
       </div>
     </section>
   );
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,9 +1,33 @@
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
-import { TextSub } from './text';
+import { TextHeading, TextSub } from './text';
 
 const Badge = ({ className, children }: ComponentBaseProps) => {
-  return <TextSub className={cn('', className)}>{children}</TextSub>;
+  return (
+    <div
+      className={cn(
+        'flex w-fit flex-row gap-sm rounded-xl border-2 border-background px-md py-sm',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
+
+const Title = ({ className, children }: ComponentBaseProps) => {
+  return <TextHeading className={cn('', className)}>{children}</TextHeading>;
+};
+
+const Image = ({ className, children }: ComponentBaseProps) => {
+  return (
+    <div className={cn('relative h-[50px] w-[50px]', className)}>
+      {children}
+    </div>
+  );
+};
+
+Badge.Title = Title;
+Badge.Image = Image;
 
 export default Badge;
