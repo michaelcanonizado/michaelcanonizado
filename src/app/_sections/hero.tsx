891f0f79e80df905f4ff7cb1bf65d0ef17aba463
unknown <michaelxaviercanonizado@gmail.com>
2024-07-23 18:27:31 +0800

    refactor: use tailwind classes instead of vanila css class

--- src/app/_sections/hero.tsx
+++ src/app/_sections/hero.tsx
@@ -3,7 +3,7 @@ import { TextBody, TextDisplay } from '@/components/ui/text';
 
 const Hero = () => {
   return (
-    <section className='hero-height mb-xl flex w-full flex-col justify-end overflow-hidden pb-md text-center'>
+    <section className='mb-xl flex h-[calc(100vh-64px)] min-h-[600px] w-full flex-col justify-end overflow-hidden pb-md text-center'>
       <div className='flex grow flex-col items-center justify-center space-y-md'>
         <div className='space-y-[-8px]'>
           <div>
