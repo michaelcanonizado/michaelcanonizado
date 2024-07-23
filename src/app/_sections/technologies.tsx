4db3c2f4ec746703c27dc5ec8bf584f26236e2f9
unknown <michaelxaviercanonizado@gmail.com>
2024-07-23 23:45:37 +0800

    feat: add infinite logo slider

--- src/app/_sections/technologies.tsx
+++ src/app/_sections/technologies.tsx
@@ -1,33 +1,113 @@
+'use client';
+
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
-import { TextDisplay } from '@/components/ui/text';
-import Badge from '@/components/ui/badge';
+import { TextHeading } from '@/components/ui/text';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 
+const technologyImages = [
+  {
+    src: 'github-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'typescript-logo.png',
+    alt: 'typescript'
+  }
+];
+
 const Technologies = ({ className }: ComponentBaseProps) => {
+  const width = 100;
+  const height = 100;
+  const animationDuration = 10;
+  const numOfImages = technologyImages.length;
+
+  const sliderWidth = `min-w-[${width * numOfImages}px]`;
+  const sliderHeight = `h-[${height}px]`;
+  const imageWidth = `w-[${width}px]`;
+  const imageHeight = `h-[${height}px]`;
+
   return (
     <section
       className={cn(
-        'min-h-[40vh] w-full space-y-lg border bg-foreground px-lg py-xl',
+        'container min-h-[40vh] space-y-lg border-x px-lg py-xl',
         className
       )}
     >
       <div className='flex flex-row items-center justify-center'>
-        <TextDisplay className='text-background'>Technologies</TextDisplay>
+        <TextHeading>Technologies</TextHeading>
       </div>
-      <div className='flex flex-row gap-md'>
-        <Badge className='text-background'>
-          <Badge.Image>
-            <div className='h-full w-full rounded-full bg-background' />
-          </Badge.Image>
-          <Badge.Title>NextJS</Badge.Title>
-        </Badge>
-        <Badge className='text-background'>
-          <Badge.Image>
-            <div className='h-full w-full rounded-full bg-background' />
-          </Badge.Image>
-          <Badge.Title>Typescript</Badge.Title>
-        </Badge>
+      <div
+        className={cn(
+          'relative flex h-[100px] flex-row overflow-hidden border-x bg-slate-300',
+          sliderHeight,
+          sliderWidth
+        )}
+      >
+        {technologyImages.map((image, index) => {
+          return (
+            <motion.div
+              className={cn(
+                'absolute h-[100px] w-[100px]',
+                imageWidth,
+                imageHeight
+              )}
+              key={index}
+              initial={{
+                left: '100%'
+              }}
+              animate={{
+                left: '-100%'
+              }}
+              transition={{
+                duration: animationDuration,
+                type: 'tween',
+                ease: 'linear',
+                repeat: Infinity,
+                delay: (animationDuration / numOfImages) * (index + 3)
+              }}
+            >
+              <Image
+                alt={image.alt}
+                src={`/technologies-temp/${image.src}`}
+                fill
+              />
+            </motion.div>
+          );
+        })}
       </div>
     </section>
   );
