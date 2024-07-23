fcb59a9c83a832f2f82ab1559e525216e37dff02
unknown <michaelxaviercanonizado@gmail.com>
2024-07-23 17:43:25 +0800

    feat: add section bleed to <Technologies/>

--- src/app/_sections/technologies.tsx
+++ src/app/_sections/technologies.tsx
@@ -3,7 +3,22 @@ import { ComponentBaseProps } from '@/types';
 import React from 'react';
 
 const Technologies = ({ className }: ComponentBaseProps) => {
-  return <section className={cn('', className)}>Technologies</section>;
+  return (
+    <section
+      className={cn(
+        'min-h-[80vh] w-full border bg-foreground py-xl',
+        className
+      )}
+      style={{
+        marginLeft: 'calc(50%-50vw)',
+        marginRight: 'calc(50%-50vw)'
+      }}
+    >
+      <div className='flex flex-row items-center justify-center'>
+        <h1 className='text-heading text-background'>Technologies</h1>
+      </div>
+    </section>
+  );
 };
 
 export default Technologies;
