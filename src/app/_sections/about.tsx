215efc9774ce0f4e9c1962138d1053b36cd34793
unknown <michaelxaviercanonizado@gmail.com>
2024-07-23 18:20:42 +0800

    refactor: use custom component base style wrapper for text

--- src/app/_sections/about.tsx
+++ src/app/_sections/about.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
-import React from 'react';
+import { TextBody, TextDisplay, TextHeading } from '@/components/ui/text';
 
 const Picture = () => {
   return (
@@ -18,30 +18,30 @@ const About = ({ className }: ComponentBaseProps) => {
         className
       )}
     >
-      <div className=''>
-        <h1 className='text-display text-center'>About</h1>
+      <div>
+        <TextDisplay className='text-center'>About</TextDisplay>
       </div>
 
       <div className='grid grid-cols-2 gap-xl'>
         <Picture />
         <div className='max-w-[450px] space-y-md'>
           <div className='mb-lg'>
-            <h1 className='text-heading'>Who am I?</h1>
+            <TextHeading>Who am I?</TextHeading>
           </div>
-          <p className='text-body'>
-            Hi there, I'm Michael {'(you can call me Mikey!)'}. I am a
-            self-taught web developer based in the Philippines.
-          </p>
-          <p className='text-body'>
+          <TextBody>
+            Hi there, I'm Michael (you can call me Mikey!). I am a self-taught
+            web developer based in the Philippines.
+          </TextBody>
+          <TextBody>
             I love building websites that are user-friendly, aesthetic, and
             functional. I am a 2nd year Computer Science student with 4 years of
             experience in web development.
-          </p>
-          <p className='text-body'>
+          </TextBody>
+          <TextBody>
             I'm here to transform your ideas into a website that elevates your
             business. My strong work ethic and discipline set me apart as a
             developer you can rely on.
-          </p>
+          </TextBody>
         </div>
       </div>
     </section>
