fb641397fbd3ab9ba9db8d45ea41855f37984116
unknown <michaelxaviercanonizado@gmail.com>
2024-07-23 18:05:41 +0800

    feat: add <TextHeading/>

---
+++ src/components/ui/text.tsx
@@ -0,0 +1,15 @@
+import { cn } from '@/lib/utils';
+import { ComponentBaseProps } from '@/types';
+
+export const TextHeading = ({ className, children }: ComponentBaseProps) => {
+  return (
+    <h2
+      className={cn(
+        'text-[calc((40/16)*1rem)] font-[700] leading-[1.1] tracking-[calc((-2.5/16)*1rem)]',
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
+};
