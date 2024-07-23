5c2bcc129017a82289bd29c5bbb1ba86f6446001
unknown <michaelxaviercanonizado@gmail.com>
2024-07-23 18:06:51 +0800

    feat: add <TextDisplay/>

--- src/components/ui/text.tsx
+++ src/components/ui/text.tsx
@@ -1,6 +1,19 @@
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
 
+export const TextDisplay = ({ className, children }: ComponentBaseProps) => {
+  return (
+    <h1
+      className={cn(
+        'text-[calc((80/16)*1rem)] font-[700] leading-[1.1] tracking-[calc((-4.5/16)*1rem)]',
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+};
+
 export const TextHeading = ({ className, children }: ComponentBaseProps) => {
   return (
     <h2
