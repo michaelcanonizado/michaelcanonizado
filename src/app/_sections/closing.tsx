9f807af41286b87122542e692daacbfde291beed
unknown <michaelxaviercanonizado@gmail.com>
2024-07-25 00:08:34 +0800

    feat: add <Card/> to beside closing paragraph

--- src/app/_sections/closing.tsx
+++ src/app/_sections/closing.tsx
@@ -1,15 +1,20 @@
 import { cn } from '@/lib/utils';
-import { TextBody, TextDisplay, TextHeading } from '@/components/ui/text';
 import { ComponentBaseProps } from '@/types';
+import { TextBody, TextDisplay, TextHeading } from '@/components/ui/text';
+import Card from '@/components/ui/card';
 
 const Closing = ({ className }: ComponentBaseProps) => {
   return (
-    <section className={cn('section min-h-[80vh] space-y-xl', className)}>
+    <section
+      className={cn('section min-h-[100vh] space-y-xl pt-[150px]', className)}
+    >
       <div className='text-center'>
         <TextDisplay>{"LET'S DO SOMETHING AWESOME TOGETHER!"}</TextDisplay>
       </div>
       <div className='grid grid-cols-2'>
-        <div className=''></div>
+        <div className='relative'>
+          <Card className='absolute bottom-[-50%] right-[50%] translate-x-[50%] rotate-[-10deg] scale-[1.1] opacity-[75%]' />
+        </div>
         <div className='max-w-[450px] space-y-md'>
           <div>
             <TextHeading>
