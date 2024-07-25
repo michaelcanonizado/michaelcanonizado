76b844afc3948c0c47aa993149cdc04b7b43135c
unknown <michaelxaviercanonizado@gmail.com>
2024-07-25 16:10:14 +0800

    feat: use <nameFirst/> instead of infinite email slider

--- src/app/_sections/footer.tsx
+++ src/app/_sections/footer.tsx
@@ -1,19 +1,18 @@
 import { cn } from '@/lib/utils';
 import { TextDisplay } from '@/components/ui/text';
 import { ComponentBaseProps } from '@/types';
+import { NameFirst } from '../../../public/name/name-first';
 
 const Footer = ({ className }: ComponentBaseProps) => {
   return (
     <section
       className={cn(
-        'section flex !w-[calc(100vw-17px)] flex-col justify-end space-y-xl overflow-hidden',
+        'section flex !w-[calc(100vw-18px)] flex-col justify-end bg-foreground !pb-0',
         className
       )}
     >
-      <div className='group border-y py-[-32px] hover:cursor-pointer hover:bg-foreground'>
-        <TextDisplay className='translate-x-[-75%] text-[200px] group-hover:text-foreground-secondary'>
-          michaelxaviercanonizado@gmail.com
-        </TextDisplay>
+      <div className='px-md pb-md'>
+        <NameFirst variant='white' />
       </div>
     </section>
   );
