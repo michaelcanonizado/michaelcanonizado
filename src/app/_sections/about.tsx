1ebf40ec29c9e2dac730381c6799e97cdf5132d5
unknown <michaelxaviercanonizado@gmail.com>
2024-07-25 20:17:35 +0800

    feat: revamp <About/> layout

--- src/app/_sections/about.tsx
+++ src/app/_sections/about.tsx
@@ -5,7 +5,7 @@ import { TextBody, TextDisplay, TextHeading } from '@/components/ui/text';
 const Picture = () => {
   return (
     <div className='overflow-hidden rounded-xl'>
-      <div className='h-[600px] w-[500px] bg-foreground' />
+      <div className='h-[700px] w-[550px] bg-foreground' />
     </div>
   );
 };
@@ -14,7 +14,7 @@ const About = ({ className }: ComponentBaseProps) => {
   return (
     <section
       className={cn(
-        'section flex min-h-[80vh] flex-col items-center space-y-xl !pb-lg',
+        'section max-height flex flex-col items-center space-y-lg',
         className
       )}
     >
@@ -22,25 +22,27 @@ const About = ({ className }: ComponentBaseProps) => {
         <TextDisplay className='text-center'>About</TextDisplay>
       </div>
 
-      <div className='grid grid-cols-2 gap-xl'>
-        <Picture />
-        <div className='max-w-[450px] space-y-md'>
-          <div className='mb-lg'>
-            <TextHeading>Who am I?</TextHeading>
-          </div>
-          <TextBody>
+      <div className='grid grid-cols-[1fr_auto_1fr] gap-lg'>
+        <div className='flex flex-col items-end justify-center gap-lg text-end'>
+          <TextHeading>
             Hi there, I'm Michael (you can call me Mikey!). I am a self-taught
             web developer based in the Philippines.
+          </TextHeading>
+          <TextBody>
+            I am a 2nd year Computer Science student with 4 years of experience
+            in web development.
           </TextBody>
+        </div>
+        <Picture />
+        <div className='flex flex-col items-start justify-center gap-md'>
           <TextBody>
             I love building websites that are user-friendly, aesthetic, and
-            functional. I am a 2nd year Computer Science student with 4 years of
-            experience in web development.
+            functional.
           </TextBody>
           <TextBody>
             I'm here to transform your ideas into a website that elevates your
             business. My strong work ethic and discipline set me apart as a
-            developer you can rely on.
+            developer you can rely on!
           </TextBody>
         </div>
       </div>
