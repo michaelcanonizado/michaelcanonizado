ccfecc5d6ab9158ca2e4a66e937ced70e56b088e
unknown <michaelxaviercanonizado@gmail.com>
2024-07-26 14:56:26 +0800

    chore: initialize <InfiniteSlider/>

---
+++ src/components/ui/infinite-slider.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { ComponentBaseProps } from '@/types';
+
+const InfiniteSlider = ({ className, children }: ComponentBaseProps) => {
+  return <div className={cn('', className)}>InfiniteSlider</div>;
+};
+
+export default InfiniteSlider;
