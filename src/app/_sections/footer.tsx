1fefa9712afcab6cf4addc6e0e175a9530cb3ed2
unknown <michaelxaviercanonizado@gmail.com>
2024-07-26 15:04:37 +0800

    feat: make inifinite email slider a reusable infinite slider component

--- src/app/_sections/footer.tsx
+++ src/app/_sections/footer.tsx
@@ -1,101 +1,17 @@
-'use client';
-
-import { useState } from 'react';
 import { cn } from '@/lib/utils';
-import {
-  motion,
-  useAnimationFrame,
-  useMotionValue,
-  useScroll,
-  useSpring,
-  useTransform,
-  useVelocity,
-  wrap
-} from 'framer-motion';
 import { TextBody, TextHeading } from '@/components/ui/text';
 import { ComponentBaseProps } from '@/types';
 import { NameFirst } from '@/../public/name/name-first';
-
-const InfiniteEmailSlider = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const baseX = useMotionValue(0);
-  const baseVelocity = 3;
-
-  /* Track scroll */
-  const { scrollY } = useScroll();
-  /* Get scroll velocity */
-  const scrollVelocity = useVelocity(scrollY);
-  /* Smoothen/add spring to scroll */
-  const smoothVelocity = useSpring(scrollVelocity, {
-    damping: 50,
-    stiffness: 400
-  });
-  /* Transform the velocity value to a smaller range */
-  const velocityFactor = useTransform(smoothVelocity, [0, 5000], [0, 10], {
-    clamp: false
-  });
-
-  const x = useTransform(baseX, v => `${wrap(-20, -45, v)}%`);
-  const directionFactor = { current: 1 };
-  useAnimationFrame((t, delta) => {
-    /* Each frame the slider will move depending on how much velocity there is in the scroll. If the slider isHovered, the moveBy value will be 0. */
-    let moveBy = 0;
-    if (isHovered === false) {
-      moveBy = directionFactor.current * baseVelocity * (delta / 1000);
-    }
-
-    /* Change direction depending on the scroll direction. */
-    if (velocityFactor.get() < 0) {
-      directionFactor.current = -1;
-    } else if (velocityFactor.get() > 0) {
-      directionFactor.current = 1;
-    }
-
-    moveBy += directionFactor.current * moveBy * velocityFactor.get();
-
-    baseX.set(baseX.get() + moveBy);
-  });
-
-  const onMouseEnter = () => {
-    setIsHovered(true);
-  };
-  const onMouseLeave = () => {
-    setIsHovered(false);
-  };
-
-  const Content = (
-    <span className='mx-lg font-display text-[100px] font-bold group-hover:text-foreground-secondary'>
-      michaelxaviercanonizado@gmail.com
-    </span>
-  );
-
-  return (
-    <>
-      <div
-        className='group flex flex-row flex-nowrap overflow-hidden border-y py-sm hover:cursor-pointer hover:bg-foreground'
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        <motion.div
-          id='slide'
-          className={cn('flex w-fit flex-row flex-nowrap')}
-          style={{ x }}
-        >
-          {Content}
-          {Content}
-          {Content}
-          {Content}
-        </motion.div>
-      </div>
-    </>
-  );
-};
+import InfiniteSlider from '@/components/ui/infinite-slider';
 
 const Footer = ({ className }: ComponentBaseProps) => {
   return (
     <section className={cn('w-full space-y-md', className)}>
-      <InfiniteEmailSlider />
+      <InfiniteSlider className='' baseVelocity={-3}>
+        <span className='mx-lg font-display text-[100px] font-bold group-hover:text-foreground-secondary'>
+          michaelxaviercanonizado@gmail.com
+        </span>
+      </InfiniteSlider>
       <div className='flex flex-col justify-end bg-foreground !py-0'>
         <div className='flex grow flex-row justify-between px-xl py-xl'>
           <div className='max-w-[500px]'>
--- src/components/ui/infinite-slider.tsx
+++ src/components/ui/infinite-slider.tsx
@@ -1,10 +1,92 @@
 'use client';
 
+import { useState } from 'react';
+
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
+import {
+  motion,
+  useAnimationFrame,
+  useMotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+  useVelocity,
+  wrap
+} from 'framer-motion';
 
-const InfiniteSlider = ({ className, children }: ComponentBaseProps) => {
-  return <div className={cn('', className)}>InfiniteSlider</div>;
+const InfiniteSlider = ({
+  className,
+  children,
+  baseVelocity = 3
+}: ComponentBaseProps & { baseVelocity?: number }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const baseX = useMotionValue(0);
+
+  /* Track scroll */
+  const { scrollY } = useScroll();
+  /* Get scroll velocity */
+  const scrollVelocity = useVelocity(scrollY);
+  /* Smoothen/add spring to scroll */
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400
+  });
+  /* Transform the velocity value to a smaller range */
+  const velocityFactor = useTransform(smoothVelocity, [0, 5000], [0, 10], {
+    clamp: false
+  });
+
+  const x = useTransform(baseX, v => `${wrap(-20, -45, v)}%`);
+  const directionFactor = { current: 1 };
+  useAnimationFrame((t, delta) => {
+    /* Each frame the slider will move depending on how much velocity there is in the scroll. If the slider isHovered, the moveBy value will be 0. */
+    let moveBy = 0;
+    if (isHovered === false) {
+      moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+    }
+
+    /* Change direction depending on the scroll direction. */
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  const onMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const onMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div
+      className={cn(
+        'group flex flex-row flex-nowrap overflow-hidden border-y p-0 hover:cursor-pointer hover:bg-foreground',
+        className
+      )}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <motion.div
+        id='slide'
+        className={cn('flex w-fit flex-row flex-nowrap')}
+        style={{ x }}
+      >
+        {children}
+        {children}
+        {children}
+        {children}
+      </motion.div>
+    </div>
+  );
 };
 
 export default InfiniteSlider;
