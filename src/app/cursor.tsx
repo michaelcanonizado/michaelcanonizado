2222a58df0f52b2ac503b3076439202989fbd7d5
unknown <michaelxaviercanonizado@gmail.com>
2024-07-30 23:17:13 +0800

    feat: only use transition on defualt variant

--- src/app/cursor.tsx
+++ src/app/cursor.tsx
@@ -2,11 +2,12 @@
 
 import { motion } from 'framer-motion';
 import { useCursorContext } from '@/context/cursor';
+import { cn } from '@/lib/utils';
 
 const Cursor = () => {
   const { cursor } = useCursorContext();
   const { x, y } = cursor.mousePosition;
-  const { isHidden } = cursor;
+  const { isHidden = true } = cursor;
 
   const size = 32;
 
@@ -14,9 +15,16 @@ const Cursor = () => {
     default: {
       scale: 1,
       x: x - size / 2,
-      y: y - size / 2
+      y: y - size / 2,
+      transition: {
+        type: 'tween',
+        ease: 'backOut',
+        duration: 0
+      }
     },
     hidden: {
+      x: x - size / 2,
+      y: y - size / 2,
       opacity: 0,
       scale: 0
     }
@@ -26,16 +34,14 @@ const Cursor = () => {
     <motion.div
       variants={variants}
       animate={isHidden ? 'hidden' : 'default'}
-      transition={{
-        type: 'tween',
-        ease: 'backOut',
-        duration: 0
-      }}
       style={{
         width: `${size}px`,
         height: `${size}px`
       }}
-      className='pointer-events-none fixed left-0 top-0 scale-[0] rounded-full bg-foreground'
+      className={cn(
+        'pointer-events-none fixed left-0 top-0 scale-0 rounded-full',
+        isHidden ? 'bg-background' : 'bg-foreground'
+      )}
     />
   );
 };
