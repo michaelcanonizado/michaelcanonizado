4fdb4544ec044412ae4d7fad517dd824004f329e
unknown <michaelxaviercanonizado@gmail.com>
2024-08-01 01:21:18 +0800

    fix: add horizontal margin

--- src/app/_sections/introduction.tsx
+++ src/app/_sections/introduction.tsx
@@ -19,37 +19,33 @@ const Introduction = ({ className }: ComponentBaseProps) => {
   const scale = useTransform(scrollYProgress, [0, 1], [1.2, 1]);
 
   return (
-    <section
-      ref={containerRef}
-      className={cn(
-        'container my-0 flex items-center justify-center space-x-xl overflow-hidden rounded-xl bg-muted py-2xl',
-        className
-      )}
-    >
-      <motion.div
-        style={{
-          y,
-          rotate: rotate,
-          scale
-        }}
-        className=''
-      >
-        <Card />
-      </motion.div>
+    <section ref={containerRef} className={cn('container my-0', className)}>
+      <div className='flex items-center justify-center space-x-xl overflow-hidden rounded-xl bg-muted py-2xl'>
+        <motion.div
+          style={{
+            y,
+            rotate: rotate,
+            scale
+          }}
+          className=''
+        >
+          <Card />
+        </motion.div>
 
-      <div className='max-w-[400px] space-y-md'>
-        <div>
-          <TextHeading variants={textBodyVariants}>
-            Looking for a developer who can create a visually appealing and
-            fully functional website?
-          </TextHeading>
-        </div>
+        <div className='max-w-[400px] space-y-md'>
+          <div>
+            <TextHeading variants={textBodyVariants}>
+              Looking for a developer who can create a visually appealing and
+              fully functional website?
+            </TextHeading>
+          </div>
 
-        <div className='max-w-[700px]'>
-          <TextBody>
-            Your search ends here! With my knowledge in web development,
-            you&apos;ll get a website that sets you apart!
-          </TextBody>
+          <div className='max-w-[700px]'>
+            <TextBody>
+              Your search ends here! With my knowledge in web development,
+              you&apos;ll get a website that sets you apart!
+            </TextBody>
+          </div>
         </div>
       </div>
     </section>
