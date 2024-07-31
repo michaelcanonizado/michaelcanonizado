7fc5af4208deeb5a2e8cb1aa5b1da799394bf800
unknown <michaelxaviercanonizado@gmail.com>
2024-08-01 01:30:25 +0800

    fix: make cards in <Closing/> responsive

--- src/app/_sections/closing.tsx
+++ src/app/_sections/closing.tsx
@@ -12,10 +12,10 @@ const Closing = ({ className }: ComponentBaseProps) => {
       )}
     >
       <div className='absolute inset-0 z-0'>
-        <Card className='absolute left-[12%] top-[-6%] rotate-[10deg] scale-[1.5] opacity-[60%]' />
-        <Card className='absolute right-[12%] top-[-5%] rotate-[-10deg] scale-[1.5] opacity-[60%]' />
-        <Card className='absolute bottom-[-2%] left-[5%] h-[250px] w-[400px] rotate-[-5deg] scale-[1.5] opacity-[60%]' />
-        <Card className='absolute bottom-[-2%] right-[5%] h-[250px] w-[400px] rotate-[5deg] scale-[1.6] opacity-[60%]' />
+        <Card className='absolute left-[12%] top-[-6%] h-[20vw] min-h-[250px] w-[14vw] min-w-[175px] rotate-[10deg] scale-[1.5] opacity-[60%]' />
+        <Card className='absolute right-[12%] top-[-5%] h-[20vw] min-h-[250px] w-[14vw] min-w-[175px] rotate-[-10deg] scale-[1.5] opacity-[60%]' />
+        <Card className='absolute bottom-[-2%] left-[5%] h-[14vw] min-h-[175px] w-[20vw] min-w-[250px] rotate-[-5deg] scale-[1.5] opacity-[60%]' />
+        <Card className='absolute bottom-[-2%] right-[5%] h-[14vw] min-h-[175px] w-[20vw] min-w-[250px] rotate-[5deg] scale-[1.6] opacity-[60%]' />
       </div>
       <div className='z-10 max-w-[1000px] text-center'>
         <TextDisplay>
