5b945ec45d094e1e4e2109eaace7493f07fde307
unknown <michaelxaviercanonizado@gmail.com>
2024-08-01 00:49:25 +0800

    refactor: move <Project/> into a separate file to keep <Projects/> a server component

--- src/app/_sections/projects.tsx
+++ src/app/_sections/projects.tsx
@@ -1,80 +1,8 @@
-'use client';
-
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
-import { motion } from 'framer-motion';
 
-import {
-  TextBody,
-  TextDisplay,
-  TextHeading,
-  TextSub
-} from '@/components/ui/text';
-import Link from '@/components/ui/link';
-
-const Project = () => {
-  const containerVariants = {
-    hidden: {
-      y: 50,
-      opacity: 0
-    },
-    show: {
-      y: 0,
-      opacity: 100,
-      transition: {
-        opacity: {
-          duration: 0.7,
-          type: 'tween',
-          ease: 'linear'
-        },
-        y: {
-          duration: 0.5,
-          type: 'spring'
-        }
-      }
-    }
-  };
-
-  return (
-    <motion.div
-      variants={containerVariants}
-      initial='hidden'
-      whileInView='show'
-      viewport={{ once: true }}
-      className='flex w-full flex-col justify-start overflow-hidden rounded-lg border bg-muted lg:flex-row'
-    >
-      <div className='h-[350px] w-full min-w-[400px] lg:max-w-[700px]'>
-        <div className='h-full w-full bg-muted' />
-      </div>
-      <div className='flex flex-col justify-between p-lg'>
-        <div className='mb-lg'>
-          <div className='mb-xs flex items-center justify-start'>
-            <TextHeading showAnimation={false}>minder</TextHeading>
-          </div>
-          <div className='mb-md flex flex-row gap-sm'>
-            <TextSub showAnimation={false} className='font-semibold'>
-              NextJS
-            </TextSub>
-            <TextSub showAnimation={false} className='font-semibold'>
-              ShadCN
-            </TextSub>
-            <TextSub showAnimation={false} className='font-semibold'>
-              MongoDB
-            </TextSub>
-          </div>
-          <div className='flex items-center justify-end'>
-            <TextBody showAnimation={false}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              nibh ligula, tempor sed elementum quis, sodales ac erat. Cras
-              rhoncus volutpat lectus nec venenatis.
-            </TextBody>
-          </div>
-        </div>
-        <Link>View</Link>
-      </div>
-    </motion.div>
-  );
-};
+import { TextDisplay, TextHeading } from '@/components/ui/text';
+import Project from '@/components/ui/project';
 
 const Projects = ({ className }: ComponentBaseProps) => {
   return (
