cd6d0b32f94b30b07d3f3cf66777c5acb8536884
unknown <michaelxaviercanonizado@gmail.com>
2024-07-31 14:54:16 +0800

    feat: revamp <Projects/> layout

--- src/app/_sections/projects.tsx
+++ src/app/_sections/projects.tsx
@@ -10,26 +10,27 @@ import Link from '@/components/ui/link';
 
 const Project = () => {
   return (
-    <div className='grid grid-cols-2 gap-lg'>
-      <div className='flex flex-row justify-end'>
-        <div className='h-[350px] w-[550px] rounded-lg bg-foreground' />
+    <div className='flex w-full flex-row justify-start overflow-hidden rounded-lg border'>
+      <div className='h-[350px] w-full min-w-[400px] max-w-[700px]'>
+        <div className='h-full w-full bg-foreground' />
       </div>
-      <div className='w-full max-w-[550px] shrink'>
-        <div className='mb-sm flex items-center justify-start'>
-          <TextHeading>minder</TextHeading>
-        </div>
-        <div className='mb-md flex flex-row gap-sm'>
-          <TextSub className='font-semibold'>NextJS</TextSub>
-          <TextSub className='font-semibold'>ShadCN</TextSub>
-          <TextSub className='font-semibold'>MongoDB</TextSub>
-        </div>
-        <div className='mb-lg flex items-center justify-end'>
-          <TextBody>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            nibh ligula, tempor sed elementum quis, sodales ac erat. Cras
-            rhoncus volutpat lectus nec venenatis. Nulla convallis nibh est, a
-            convallis dolor venenatis id.
-          </TextBody>
+      <div className='flex flex-col justify-between p-lg'>
+        <div className=''>
+          <div className='mb-xs flex items-center justify-start'>
+            <TextHeading>minder</TextHeading>
+          </div>
+          <div className='mb-md flex flex-row gap-sm'>
+            <TextSub className='font-semibold'>NextJS</TextSub>
+            <TextSub className='font-semibold'>ShadCN</TextSub>
+            <TextSub className='font-semibold'>MongoDB</TextSub>
+          </div>
+          <div className='flex items-center justify-end'>
+            <TextBody>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              nibh ligula, tempor sed elementum quis, sodales ac erat. Cras
+              rhoncus volutpat lectus nec venenatis.
+            </TextBody>
+          </div>
         </div>
         <Link>View</Link>
       </div>
@@ -43,7 +44,7 @@ const Projects = ({ className }: ComponentBaseProps) => {
       <div className=''>
         <TextDisplay className='text-center'>Projects</TextDisplay>
       </div>
-      <div className='flex flex-col gap-xl'>
+      <div className='flex flex-col gap-lg'>
         <Project />
         <Project />
         <Project />
