b44f8823d34759efce4112d5d18872270da7aa80
unknown <michaelxaviercanonizado@gmail.com>
2024-07-31 22:20:21 +0800

    feat: modify lenis scroll options

--- src/providers/lenis.tsx
+++ src/providers/lenis.tsx
@@ -1,9 +1,22 @@
 'use client';
 
-import { ReactLenis, useLenis } from 'lenis/react';
+import { ReactLenis } from 'lenis/react';
 
 const LenisScrollProvider = ({ children }: { children?: React.ReactNode }) => {
-  return <ReactLenis root>{children}</ReactLenis>;
+  return (
+    <ReactLenis
+      root
+      options={{
+        duration: 2,
+        easing: t => {
+          return t === 1 ? 1 : 1 - Math.pow(2, -10 * t);
+        },
+        wheelMultiplier: 1.2
+      }}
+    >
+      {children}
+    </ReactLenis>
+  );
 };
 
 export default LenisScrollProvider;
