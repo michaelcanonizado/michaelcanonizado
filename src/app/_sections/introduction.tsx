7dc5955df277df4229ae1435674be4d19f9db952
unknown <michaelxaviercanonizado@gmail.com>
2024-07-31 15:13:00 +0800

    feat: add scrolling effect to card in <Introduction/>

--- src/app/_sections/introduction.tsx
+++ src/app/_sections/introduction.tsx
@@ -1,19 +1,40 @@
+'use client';
+
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
 import { TextBody, TextHeading } from '@/components/ui/text';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Card from '@/components/ui/card';
+import { useRef } from 'react';
 
 const Introduction = ({ className }: ComponentBaseProps) => {
+  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end', 'end 70%']
+  });
+
+  const rotate = useTransform(scrollYProgress, [0, 1], [10, -10]);
+  const y = useTransform(scrollYProgress, [0, 1], [-150, 0]);
+
   return (
     <section
+      ref={containerRef}
       className={cn(
-        'container my-0 flex items-center justify-center space-x-xl border-y py-2xl',
+        'container my-0 flex items-center justify-center space-x-xl overflow-hidden border-y py-2xl',
         className
       )}
     >
-      <div className='rotate-[10deg]'>
+      <motion.div
+        style={{
+          y,
+          rotate: rotate
+        }}
+        className=''
+      >
         <Card />
-      </div>
+      </motion.div>
+
       <div className='max-w-[400px] space-y-md'>
         <div>
           <TextHeading>
