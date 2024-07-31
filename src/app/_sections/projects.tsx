2e5df89400fafc7e73d897d15efd101e59975000
unknown <michaelxaviercanonizado@gmail.com>
2024-07-31 14:57:38 +0800

    feat: add short description beneath <Projects/> header

--- src/app/_sections/projects.tsx
+++ src/app/_sections/projects.tsx
@@ -41,8 +41,16 @@ const Project = () => {
 const Projects = ({ className }: ComponentBaseProps) => {
   return (
     <section className={cn('container space-y-xl', className)}>
-      <div className=''>
-        <TextDisplay className='text-center'>Projects</TextDisplay>
+      <div className='flex flex-col items-center space-y-lg'>
+        <div className=''>
+          <TextDisplay className='text-center'>Projects</TextDisplay>
+        </div>
+        <div className='max-w-[600px]'>
+          <TextHeading className='text-center'>
+            I really enjoy working on side projects. Wearing multiple hats as a
+            designer, thinker, and a manager!
+          </TextHeading>
+        </div>
       </div>
       <div className='flex flex-col gap-lg'>
         <Project />
