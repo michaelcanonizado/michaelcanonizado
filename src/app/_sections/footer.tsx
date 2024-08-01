f74cc1a8dc8a4f1f722caec964c1e666a25a668e
unknown <michaelxaviercanonizado@gmail.com>
2024-08-01 18:35:12 +0800

    feat: revamp <Footer/>

--- src/app/_sections/footer.tsx
+++ src/app/_sections/footer.tsx
@@ -51,36 +51,38 @@ const Footer = ({ className }: ComponentBaseProps) => {
 
   return (
     <section className={cn('w-full space-y-md', className)}>
-      <InfiniteSlider className='' baseVelocity={-3}>
-        <span className='mx-lg font-display text-[100px] font-bold group-hover:text-foreground-secondary'>
-          {email}
-        </span>
-      </InfiniteSlider>
-      <div className='flex flex-col justify-end bg-muted !py-0'>
-        <div className='flex grow flex-row justify-between px-xl py-xl'>
-          <div className='max-w-[500px]'>
-            <TextHeading className={textColor}>{text.heading}</TextHeading>
-          </div>
-          <div className='flex flex-row gap-xl'>
-            {links.map((linkGroup, index) => {
-              return (
-                <div className='space-y-md' key={index}>
-                  <div className=''>
-                    <TextHeading className={textColor}>
-                      {linkGroup.heading}
-                    </TextHeading>
+      <div className='max-height !m-lg flex flex-col justify-end rounded-xl bg-muted py-lg'>
+        <div className='flex grow flex-col justify-between'>
+          <InfiniteSlider className='' baseVelocity={-3}>
+            <span className='mx-lg font-display text-[100px] font-bold group-hover:text-foreground-secondary'>
+              {email}
+            </span>
+          </InfiniteSlider>
+          <div className='flex flex-row justify-between gap-lg px-lg py-xl'>
+            <div className='max-w-[500px]'>
+              <TextHeading className={textColor}>{text.heading}</TextHeading>
+            </div>
+            <div className='flex flex-row gap-xl'>
+              {links.map((linkGroup, index) => {
+                return (
+                  <div className='h-fit space-y-md' key={index}>
+                    <div className=''>
+                      <TextHeading className={textColor}>
+                        {linkGroup.heading}
+                      </TextHeading>
+                    </div>
+                    <div className={textColor}>
+                      {linkGroup.links.map((link, index) => {
+                        return <TextBody key={index}>{link.name}</TextBody>;
+                      })}
+                    </div>
                   </div>
-                  <div className={textColor}>
-                    {linkGroup.links.map((link, index) => {
-                      return <TextBody key={index}>{link.name}</TextBody>;
-                    })}
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
-        <div className='px-md pb-md'>
+        <div className='px-lg'>
           <NameFirst className='fill-brand' />
         </div>
       </div>
