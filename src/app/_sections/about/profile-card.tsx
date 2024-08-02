1368f66508ea565d050c8f3b75d02906dba39a83
unknown <michaelxaviercanonizado@gmail.com>
2024-08-02 18:08:17 +0800

    feat: add address to <ProfileCard/>

--- src/app/_sections/about/profile-card.tsx
+++ src/app/_sections/about/profile-card.tsx
@@ -28,6 +28,23 @@ const GitCommit = ({ className }: ComponentBaseProps) => {
     </svg>
   );
 };
+const MapPin = ({ className }: ComponentBaseProps) => {
+  return (
+    <svg
+      className={className}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='1'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    >
+      <path d='M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0' />
+      <circle cx='12' cy='10' r='3' />
+    </svg>
+  );
+};
 
 const ProfileCard = ({ className }: ComponentBaseProps) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -81,9 +98,18 @@ const ProfileCard = ({ className }: ComponentBaseProps) => {
             <VerifiedBadge className='h-[24px]' />
           </div>
 
-          <TextSub>@michaelcanonizado</TextSub>
+          <TextSub className='font-light tracking-widest'>
+            @michaelcanonizado
+          </TextSub>
 
-          <div className='glex-row flex items-center gap-xs'>
+          <div className='glex-row ml-[-2px] flex items-center'>
+            <MapPin className='h-[16px] w-[16px]' />
+            <TextSub className='font-light tracking-widest'>
+              Albay, Philippines
+            </TextSub>
+          </div>
+
+          <div className='mt-sm flex flex-row items-center gap-xs'>
             <GitCommit className='h-[20px] w-[20px]' />
             <TextSub>{commitCount.toLocaleString('en-US')} commits</TextSub>
           </div>
