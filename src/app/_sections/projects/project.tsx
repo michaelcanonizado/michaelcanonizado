4a65c672e04b4f2928c75ea3ebbc13d2e0ad80cd
unknown <michaelxaviercanonizado@gmail.com>
2024-08-02 17:07:43 +0800

    feat: add magnetic animation to view project external link

--- src/app/_sections/projects/project.tsx
+++ src/app/_sections/projects/project.tsx
@@ -2,10 +2,11 @@
 
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
-import { motion } from 'framer-motion';
+import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion';
 
 import { TextBody, TextHeading, TextSub } from '@/components/text';
 import Image from 'next/image';
+import { MouseEventHandler, useRef } from 'react';
 
 const ExternalLink = ({ className }: ComponentBaseProps) => {
   return (
@@ -27,20 +28,68 @@ const ExternalLink = ({ className }: ComponentBaseProps) => {
 };
 
 const Link = ({ className, url }: { url: string } & ComponentBaseProps) => {
+  const containerRef = useRef<HTMLAnchorElement | null>(null);
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const xSpring = useSpring(x);
+  const ySpring = useSpring(y);
+
+  const onMouseMove: MouseEventHandler = event => {
+    if (!containerRef.current) {
+      return;
+    }
+
+    const { clientX, clientY } = event;
+    const { width, height, left, top } =
+      containerRef.current.getBoundingClientRect();
+
+    const mouseX = (clientX - left) / width - 0.5;
+    const mouseY = (clientY - top) / height - 0.5;
+
+    x.set(mouseX);
+    y.set(mouseY);
+  };
+
+  const onMouseLeave: MouseEventHandler = event => {
+    x.set(0);
+    y.set(0);
+  };
+
+  const moveRingXBy = useTransform(xSpring, [-0.5, 0.5], [-10, 10]);
+  const moveRingYBy = useTransform(ySpring, [-0.5, 0.5], [-5, 5]);
+  const moveTextXBy = useTransform(xSpring, [-0.5, 0.5], [-6, 6]);
+  const moveTextYBy = useTransform(ySpring, [-0.5, 0.5], [-3, 3]);
+
+  const transition = {
+    type: 'spring',
+    stiffness: 200
+  };
+
   return (
-    <a
+    <motion.a
       href={url}
+      ref={containerRef}
+      onMouseMove={onMouseMove}
+      onMouseLeave={onMouseLeave}
+      style={{ x: moveTextXBy, y: moveTextYBy }}
+      transition={transition}
       target='_blank'
       className={cn(
-        'group flex w-fit flex-row items-center gap-xs rounded-bl-[50%] rounded-br-[50%] rounded-tl-[50%] rounded-tr-[50%] border-2 border-brand px-lg py-md hover:bg-background/50',
+        'group relative flex w-fit flex-row items-center gap-xs px-lg py-md',
         className
       )}
     >
+      <motion.div
+        style={{ x: moveRingXBy, y: moveRingYBy }}
+        transition={transition}
+        className='absolute inset-0 rounded-bl-[50%] rounded-br-[50%] rounded-tl-[50%] rounded-tr-[50%] border-2 border-brand bg-transparent'
+      />
       <TextSub showAnimation={false} className='mb-[-2px]'>
         View
       </TextSub>
       <ExternalLink className='h-[16px]] w-[16px]' />
-    </a>
+    </motion.a>
   );
 };
 
