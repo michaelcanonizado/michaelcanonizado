0c897341754685ca8ee14ab706e6c626b9753a70
unknown <michaelxaviercanonizado@gmail.com>
2024-08-04 23:22:18 +0800

    feat: add <AnchorLink/> component

---
+++ src/components/anchor-link.tsx
@@ -0,0 +1,23 @@
+import { ComponentBaseProps } from '@/types';
+import { cn } from '@/lib/utils';
+import { TextBody } from './text';
+
+export const AnchorLink = ({
+  className,
+  children,
+  scrollTo
+}: { scrollTo: string } & ComponentBaseProps) => {
+  return (
+    <TextBody
+      onClick={() => {
+        document.getElementById(scrollTo)?.scrollIntoView();
+      }}
+      showAnimation={false}
+      className={cn('font-display font-[500] hover:cursor-pointer', className)}
+    >
+      {children}
+    </TextBody>
+  );
+};
+
+export default AnchorLink;
