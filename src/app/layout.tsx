03e1f111296dbf0e3dd996539fff453ba4bd7d06
unknown <michaelxaviercanonizado@gmail.com>
2024-08-05 23:58:36 +0800

    chore: initialize <Loading/>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,12 @@ import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 import { inter, clashDisplay } from '@/styles/fonts';
-import NavigationBar from '@/app/_sections/navigation-bar';
-import Cursor from './cursor';
 import CursorContextProvider from '@/context/cursor';
 import LenisScrollProvider from '@/providers/lenis';
+
+import NavigationBar from '@/app/_sections/navigation-bar';
+import Cursor from './cursor';
+import Loading from './loading';
 
 export const metadata: Metadata = {
   title: 'Michael Canonizado',
@@ -18,17 +20,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className='dark min-h-screen bg-background text-foreground'>
+    <html lang='en' className='dark bg-background text-foreground'>
       {/* bg-red-500 xs:bg-orange-500 sm:bg-yellow-500 md:bg-green-500 lg:bg-blue-500  */}
       <body
         className={cn(
-          'relative flex flex-col justify-center font-sans',
+          'relative flex min-h-screen flex-col justify-center font-sans',
           inter.variable,
           clashDisplay.variable
         )}
       >
         <LenisScrollProvider>
           <CursorContextProvider>
+            <Loading />
             {/* <Cursor /> */}
             <NavigationBar />
             {children}
---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import { TextHeading } from '@/components/text';
+
+const Loading = () => {
+  return (
+    <div className='sticky inset-0 top-0 z-[99] grid h-screen place-items-center bg-muted'>
+      <TextHeading>Hello</TextHeading>
+    </div>
+  );
+};
+
+export default Loading;
