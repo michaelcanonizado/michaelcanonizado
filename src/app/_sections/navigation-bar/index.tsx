c9d2b1c9e50cb30a8651be70a0fe93be3cd96245
unknown <michaelxaviercanonizado@gmail.com>
2024-08-06 23:09:05 +0800

    feat: hide <NavigationBar/> when user reaches <Footer/>

--- src/app/_sections/navigation-bar/index.tsx
+++ src/app/_sections/navigation-bar/index.tsx
@@ -1,31 +1,78 @@
+'use client';
+
+import { useState } from 'react';
+
 import { cn } from '@/lib/utils';
 import { AnchorLinkType, ComponentBaseProps } from '@/types';
 
 import AnchorLink from '@/components/anchor-link';
 import SideMenu from './side-menu';
 import { Logo } from '@/../public/navigation-bar/logo';
+import {
+  useMotionValueEvent,
+  useScroll,
+  motion,
+  Variants
+} from 'framer-motion';
+
+const links: AnchorLinkType[] = [
+  {
+    name: 'Projects',
+    id: '#projects'
+  },
+  {
+    name: 'About',
+    id: '#about'
+  },
+  {
+    name: 'Experience',
+    id: '#experience'
+  }
+];
 
 const NavigationBar = ({ className }: ComponentBaseProps) => {
-  const links: AnchorLinkType[] = [
-    {
-      name: 'Projects',
-      id: '#projects'
-    },
-    {
-      name: 'About',
-      id: '#about'
-    },
-    {
-      name: 'Experience',
-      id: '#experience'
+  const [isHidden, setIsHidden] = useState(false);
+  const { scrollYProgress } = useScroll();
+
+  useMotionValueEvent(scrollYProgress, 'change', () => {
+    if (scrollYProgress.get() >= 0.995) {
+      setIsHidden(true);
+    } else {
+      setIsHidden(false);
     }
-  ];
+  });
+
+  const enterEase = [0.25, 1, 0.5, 1];
+  const exitEase = [0.61, 1, 0.88, 1];
+  const navigationBarVariants: Variants = {
+    initial: {
+      y: '0%'
+    },
+    hidden: {
+      y: '-100%',
+      transition: {
+        ease: exitEase,
+        duration: 0.8
+      }
+    },
+    show: {
+      y: '0%',
+      transition: {
+        ease: enterEase,
+        duration: 0.8
+      }
+    }
+  };
 
   return (
-    <header
+    <motion.header
+      variants={navigationBarVariants}
+      initial='initial'
+      animate={isHidden ? 'hidden' : 'show'}
       className={cn(
         'sticky top-0 z-50 flex flex-row justify-center',
-        className
+        className,
+        isHidden ? 'translate-y-[-100%]' : 'translate-y-0'
       )}
     >
       <nav
@@ -47,7 +94,7 @@ const NavigationBar = ({ className }: ComponentBaseProps) => {
         </div>
         <SideMenu links={links} className='block: md:hidden' />
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
