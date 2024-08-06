198ea27ce61d8b3e9981bb2dd83680c817e17868
unknown <michaelxaviercanonizado@gmail.com>
2024-08-06 16:12:28 +0800

    feat: add text animation to <Loading/>

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,71 @@
-import { TextHeading } from '@/components/text';
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { AnimatePresence, motion } from 'framer-motion';
+
+import { TextDisplay, TextHeading } from '@/components/text';
+
+const textList = [
+  'Hello',
+  '你好',
+  'Hola',
+  'नमस्ते',
+  'Bonjour',
+  'مرحبا',
+  'Привет',
+  'Hallo',
+  'Ciao',
+  'Kamusta'
+];
 
 const Loading = () => {
+  const [textIndex, setTextIndex] = useState(0);
+
+  useEffect(() => {
+    if (textIndex >= textList.length - 1) {
+      return;
+    }
+
+    const interval = textIndex === 0 ? 1000 : 200;
+
+    const timeout = setTimeout(() => {
+      setTextIndex(previousIndex => previousIndex + 1);
+    }, interval);
+
+    return () => clearInterval(timeout);
+  }, [textIndex]);
+
   return (
-    <div className='sticky inset-0 top-0 z-[99] grid h-screen place-items-center bg-muted'>
-      <TextHeading>Hello</TextHeading>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        exit={{
+          y: '-100%'
+        }}
+        transition={{
+          duration: 1
+        }}
+        className='sticky inset-0 top-0 z-[99] grid h-screen place-items-center bg-muted'
+      >
+        <motion.div
+          initial={{
+            opacity: 0
+          }}
+          animate={{
+            opacity: 100
+          }}
+          transition={{
+            duration: 1
+          }}
+          className='flex flex-row items-center justify-center gap-lg'
+        >
+          <div className='size-[36px] rounded-full bg-foreground' />
+          <TextDisplay showAnimation={false} className='text-foreground'>
+            {textList[textIndex]}
+          </TextDisplay>
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
