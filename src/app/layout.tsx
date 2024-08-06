c70e2f7e441c4f8a75180d3954e1dbf40e84fc2d
unknown <michaelxaviercanonizado@gmail.com>
2024-08-06 22:13:28 +0800

    chore: temporarily remove <LoadingScreen/>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
       >
         <LenisScrollProvider>
           <CursorContextProvider>
-            <LoadingScreen />
+            {/* <LoadingScreen /> */}
             {/* <Cursor /> */}
             <NavigationBar />
             {children}
