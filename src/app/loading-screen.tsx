3c6d1e7ad816de071f7229bd031243d7e31f9d14
unknown <michaelxaviercanonizado@gmail.com>
2024-08-06 18:08:03 +0800

    feat: add bending curve animation on <LoadingScreen/> exit

--- src/app/loading-screen.tsx
+++ src/app/loading-screen.tsx
@@ -2,9 +2,10 @@
 
 import { useEffect, useState } from 'react';
 
-import { animate, AnimatePresence, motion } from 'framer-motion';
+import { cn } from '@/lib/utils';
+import { motion, Variants } from 'framer-motion';
 
-import { TextDisplay, TextHeading } from '@/components/text';
+import { TextDisplay } from '@/components/text';
 
 const textList = [
   'Hello',
@@ -25,6 +26,8 @@ const LoadingScreen = () => {
 
   const initialDelay = 1000;
   const intervalDelay = 200;
+
+  const easing = [0.5, 0, 0.75, 0];
 
   useEffect(() => {
     if (textIndex >= textList.length - 1) {
@@ -49,25 +52,45 @@ const LoadingScreen = () => {
       y: '0%'
     },
     hidden: {
-      y: '-100%',
+      y: `-150%`,
       transition: {
-        ease: [0.5, 0, 0.75, 0],
-        duration: 0.8
+        ease: easing,
+        duration: 1.5
       }
     }
   };
 
+  const curveFlatPath = `M1000 0H0C0 0 98.5 150 501 150C883.5 150 1000 0 1000 0`;
+  const curveCurvedPath = `M500 0H0C0 0 0 10 500 20C883.5 10 1000 0 1000 0`;
+
+  const curveVariants: Variants = {
+    initial: {
+      d: curveFlatPath
+    },
+    hidden: {
+      d: curveCurvedPath,
+      transition: {
+        ease: easing,
+        delay: 0.2,
+        duration: 1
+      }
+    },
+    show: {
+      d: curveFlatPath
+    }
+  };
+
   return (
-    <AnimatePresence>
-      <motion.div
-        variants={loadingScreenVariants}
-        initial='initial'
-        animate={isComplete ? 'hidden' : 'show'}
-        transition={{
-          duration: 0.2
-        }}
-        className='fixed inset-0 top-0 z-[99] grid h-screen place-items-center bg-muted'
-      >
+    <motion.div
+      variants={loadingScreenVariants}
+      initial='initial'
+      animate={isComplete ? 'hidden' : 'show'}
+      transition={{
+        duration: 0.2
+      }}
+      className='fixed inset-0 top-0 z-[99] translate-y-[-30%]'
+    >
+      <div className='grid h-screen place-items-center bg-muted'>
         <motion.div
           initial={{
             opacity: 0
@@ -85,8 +108,11 @@ const LoadingScreen = () => {
             {textList[textIndex]}
           </TextDisplay>
         </motion.div>
-      </motion.div>
-    </AnimatePresence>
+      </div>
+      <svg className={cn('w-full', `fill-muted`)} viewBox='0 0 1000 150'>
+        <motion.path variants={curveVariants} />
+      </svg>
+    </motion.div>
   );
 };
 
