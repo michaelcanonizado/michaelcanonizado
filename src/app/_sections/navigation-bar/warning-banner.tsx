06cba238f3fe4a651ecf4394a6ab546918a300b3
unknown <michaelxaviercanonizado@gmail.com>
2024-08-20 22:38:12 +0800

    feat: add <WarningBanner/> styles

--- src/app/_sections/navigation-bar/warning-banner.tsx
+++ src/app/_sections/navigation-bar/warning-banner.tsx
@@ -1,8 +1,15 @@
 import { cn } from '@/lib/utils';
 import { ComponentBaseProps } from '@/types';
+import { TextSub } from '@/components/text';
 
 const WarningBanner = ({ className }: ComponentBaseProps) => {
-  return <div className={cn('', className)}>WarningBanner</div>;
+  return (
+    <div className={cn('min-h-[64px] bg-foreground p-md', className)}>
+      <TextSub className='flex flex-row items-center font-medium text-background'>
+        Open on desktop or increase the window size for the full experience.
+      </TextSub>
+    </div>
+  );
 };
 
 export default WarningBanner;
